f4166fdf44717b79bb0811c84d54712f9d916e44
waqasnu <waqasnu>
2016-09-17 12:48:03 +0500

    waqasnu

---
+++ Assets/PingPongKing/Scripts/DestroyByBoundry.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// This script destroys the ball game object when it leaves game area
+/// </summary>
+public class DestroyByBoundry : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D other) {
+		//On entering the boundries outside the game area the ball prefab will destroy
+		Destroy(other.gameObject);
+	}
+}
